a32889c1f3d452068c7d594e8a9e63fa91a783ba
Bebeto <robertohenriqueduarte05@gmail.com>
2025-03-17 10:51:00 -0300

    criado a Controller-aula2

---
+++ src/main/java/com/dev/Cadastro/Controller.java
@@ -0,0 +1,14 @@
+package com.dev.Cadastro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/boasVindas")
+    public  String boasVindas() {
+        return "Essa é minha 1° mensagem nessa rota!";
+    }
+}
