a19a94d749b39defc22649b09fbe6e87cab4a5c1
Bebeto <robertohenriqueduarte05@gmail.com>
2025-04-07 09:08:37 -0300

    feat: Adiciona o ProprietariosReporitory e extende o JPA .

---
+++ src/main/java/com/dev/Cadastro/Proprietarios/ProprietarioRepository.java
@@ -0,0 +1,6 @@
+package com.dev.Cadastro.Proprietarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProprietarioRepository extends JpaRepository <ProprietarioModel, Long> {
+}
