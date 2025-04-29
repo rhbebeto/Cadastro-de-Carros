840cfbf13b005930c9ea265a7c50b489b35750a4
Bebeto <robertohenriqueduarte05@gmail.com>
2025-04-29 11:20:32 -0300

    feat: Adiciona os endpoints da controller de carros

--- src/main/java/com/dev/Cadastro/Proprietarios/ProprietarioController.java
+++ src/main/java/com/dev/Cadastro/Proprietarios/ProprietarioController.java
@@ -1,14 +1,47 @@
 package com.dev.Cadastro.Proprietarios;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 public class ProprietarioController {
     @GetMapping("/boasVindas")
     public  String boasVindas() {
+
         return "Essa é minha 1° mensagem nessa rota!";
+    }
+
+    //Criar
+    @PostMapping("/criar")
+    public String criar() {
+        return "proprietario criado";
+    }
+
+
+    //Mostrar
+    @GetMapping("/todos")
+    public String mostrarTodosProprietario(){
+
+        return "mostrados";
+
+    }
+
+    //procurar
+    @GetMapping("/todosID")
+    public String mostrarTodosProprietarioID(){
+
+        return "mostrados";
+
+    }
+    //alter
+    @PutMapping("/alterarID")
+    public String alterarProprietarioID(){
+        return "Altereado";
+    }
+
+    //deletar
+    @DeleteMapping("/deletarID")
+    public String deletarId(){
+        return "Deletar";
     }
 }
