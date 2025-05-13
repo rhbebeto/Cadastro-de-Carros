b9c97538c16f9754964d5328d05b611b2d2a79f7
Bebeto <robertohenriqueduarte05@gmail.com>
2025-05-13 13:03:45 -0300

    Chore: Inclui a versao do DTO pela entidade e mapeia manualmente pelo CarroMapper e os caminhos Entidade X DTO

--- src/main/java/com/dev/Cadastro/Carros/CarroDTO.java
+++ src/main/java/com/dev/Cadastro/Carros/CarroDTO.java
@@ -1,4 +1,23 @@
 package com.dev.Cadastro.Carros;
 
+import com.dev.Cadastro.Proprietarios.ProprietarioModel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class CarroDTO {
+
+    private Long id;
+    private String modelo;
+    private int anoFabricacao;
+    private String marca;
+    private String placa;
+    private ProprietarioModel proprietario;
+    private String cor;
+
 }
